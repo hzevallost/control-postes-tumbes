fc05cd8c4f463fe608a61d64f63f2ad41df4ea3d
hzevallost <loscheva@gmail.com>
2026-09-15 18:36:32 -0500

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -83,7 +83,7 @@ col_logo, col_title = st.columns([1, 3.5])
 
 with col_logo:
     try:
-        st.image("quantum.png", width=230)
+        st.image("quantum.png", width=530)
     except:
         st.write("Logo Quantum no encontrado")
 
